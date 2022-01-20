adf3ab42c4641662050e8f6ef540a664824ad8bc
Jesse Pantoja <pantojabruin@gmail.com>
2022-01-19 19:34:36 -0800

    Add core files and basic operation functions

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+function add(a, b){
+    return Number(a) + Number(b)
+
+}
+
+function subtract(a, b){
+    return Number(a) - Number(b)
+    
+}
+
+function multiply(a, b){
+    return Number(a) * Number(b)
+    
+}
+
+function divide(a, b){
+    return Number(a) / Number(b)
+    
+}
