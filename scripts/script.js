83fa5a87ce201ff5f51e334a00733caf4f13e447
Jesse Pantoja <pantojabruin@gmail.com>
2022-01-20 02:31:56 -0800

    Implement negate functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 
-let displayValueCharacterArray = []
-let currentDisplayStringValue = ""
-let firstNum = null
-let secondNum = null
-let lastKnownOperator = null
-const calculatorContainer = document.querySelector('.calculator-container')
-const display = document.querySelector('.display')
+let displayValueCharacterArray = [];
+let currentDisplayStringValue = "";
+let firstNum = null;
+let secondNum = null;
+let lastKnownOperator = null;
+const calculatorContainer = document.querySelector('.calculator-container');
+const display = document.querySelector('.display');
 
 const mathOperationFunctions = {
     '+': (a, b) => Number(a) + Number(b),
@@ -23,12 +23,28 @@ const transformingFunctions = {
         secondNum = null;
         lastKnownOperator = null;
     }, 
+    negate: () => {
+        if (displayValueCharacterArray.length === 0) return
+        if (displayValueCharacterArray[0] !== '-'){
+            displayValueCharacterArray.unshift('-');
+            updateDisplay()
+        } else {
+            displayValueCharacterArray.splice(0, 1);
+            updateDisplay()
+
+        };     
+    },
 }
 
 function operate(operator, a, b){
     let fn = mathOperationFunctions[operator];
     return fn(a, b);
 }
+
+function updateDisplay() {
+    currentDisplayStringValue = displayValueCharacterArray.join('');
+    display.textContent = currentDisplayStringValue;
+};
 
 function containerClickHandler (e) {
     if (e.target.tagName !== 'BUTTON') return
@@ -39,18 +55,19 @@ function containerClickHandler (e) {
     else if (buttonDataValue in mathOperationFunctions) console.log(buttonDataValue);
     else transformingFunctions[buttonDataValue]()
 
-}
+};
 
 function handleClickedNumber (integerString) {
+
+    let currValueLength = displayValueCharacterArray.length
     
     // If display is currently empty i.e. 0 and 0 button was pressed, do nothing
-    if (displayValueCharacterArray.length === 0 && integerString === '0') return;
-    // Max of 9 numbers allowed on calculator display
-    if (displayValueCharacterArray.length === 9) return
+    if (currValueLength === 0 && integerString === '0') return;
+    // Max 10 characters allowed if number is negative otherwise 9
+    if ((displayValueCharacterArray[0] === '-' && currValueLength === 10) || (displayValueCharacterArray[0] !== '-' && currValueLength === 9)) return
 
     displayValueCharacterArray.push(integerString);
-    currentDisplayStringValue = displayValueCharacterArray.join("");
-    display.textContent = currentDisplayStringValue;
+    updateDisplay()
 
 };
 
